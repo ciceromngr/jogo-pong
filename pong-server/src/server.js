4018bab69240eecc249e21cfb254825c7f0e8e78
ciceromngr <cicerotransformice@gmail.com>
2021-10-26 07:36:03 -0300

    Cors

--- pong-server/src/server.js
+++ pong-server/src/server.js
@@ -7,8 +7,16 @@ import { Server } from 'socket.io'
 
 const app = express()
 const httpServer = http.createServer(app)
-const io = Server(httpServer)
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.HOST_FRONT
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+})
 
 const PORT = process.env.EXPRESS_PORT || 3333
 
-app.listen(PORT, () => console.log(`Server is running on port:${PORT}`))
+httpServer.listen(PORT, () => console.log(`Server is running on port:${PORT}`))
